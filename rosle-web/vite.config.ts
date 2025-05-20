1741214067bcafa9a2ef50547a851609be1b1cb4
Nabil Enayet <nabil@Nabils-MacBook-Pro.local>
2025-05-19 23:15:30 -0400

    Add GA with radar

--- rosle-web/vite.config.ts
+++ rosle-web/vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    VitePluginRadar({ 
+      analytics: {
+        id: 'G-LDZEXV1C8G'
+      },
+      enableDev: false
+    })
+  ],
   server: {
     host: true
   }
